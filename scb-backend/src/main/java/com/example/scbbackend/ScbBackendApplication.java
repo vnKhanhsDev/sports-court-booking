83c6413c9be070db74106f4994d4d9101277e09d
khanhvnm <namkhanhs.dev@gmail.com>
2025-12-01 21:54:13 +0700

    chore(backend): initial backend setup

---
+++ scb-backend/src/main/java/com/example/scbbackend/ScbBackendApplication.java
@@ -0,0 +1,17 @@
+package com.example.scbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class ScbBackendApplication {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+
+        SpringApplication.run(ScbBackendApplication.class, args);
+    }
+
+}
---
+++ scb-backend/src/test/java/com/example/scbbackend/ScbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
